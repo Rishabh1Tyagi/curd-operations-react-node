c1c41fef1b836308b888b3bacbbf2c7c657afc31
Rishabh1 <rishav.tyagi19@gmail.com>
2021-07-22 13:10:41 +0530

    curd-operations-react&node

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, Typography } from "@material-ui/core";
+
+const MainPage = () => {
+    return(
+         <>
+         <Box>
+          <Typography>All Data is stored in MongoDb with the help of Node js And React JS</Typography>
+         </Box>
+         </>
+    )
+}
+
+
+export default MainPage;
